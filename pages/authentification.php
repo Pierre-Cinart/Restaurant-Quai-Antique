d45095988ee28654f109bf9dbe960b5102b35bf3
Pierre-Cinart <cinartpierre@gmail.com>
2024-02-29 16:41:11 +0100

    integration formulaire de connexion front (API à revoir)

--- pages/authentification.php
+++ pages/authentification.php
@@ -7,9 +7,36 @@ require_once ('../components/navBar.php');
 
 ?>
 
-<p>page d' authentification </p>
+<div class="my-form">
+    <!-- Formulaire de connexion -->
+<form action="../back/api/login.php" method="POST">
+    <h2>Connexion</h2>
+    <label for="e-mail">Nom d'utilisateur :</label>
+    <input type="mail" id="e-mail" name="e-mail" required>
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password" required>
+    <button type="submit">Se connecter</button>
+</form>
+<div class="sep"></div>
+<!-- Formulaire d'inscription -->
+<form action="../back/api/register.php" method="POST">
+    <h2>Inscription</h2>
+    <label for="first_name">Nom d'utilisateur :</label>
+    <input type="text" id="first_name" name="first_name" required>
+    <label for="last_name">Nom d'utilisateur :</label>
+    <input type="text" id="last_name" name="last_name" required>
+    <label for="mail">Nom d'utilisateur :</label>
+    <input type="mail" id="mail" name="mail" required>
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password" required>
+    <label for="confirm_password">Confirmer le mot de passe :</label>
+    <input type="password" id="confirm_password" name="confirm_password" required>
+    <button type="submit">S'inscrire</button>
+</form>
+</div>
 
 <?php 
+
 // import du footer !!!! obligatoire pour fermer le html
 require_once ('../components/footer.php');
 
