4a84be758f9c1fba3583563fa7bafecade06e28f
Pierre-Cinart <cinartpierre@gmail.com>
2024-03-14 19:48:09 +0100

    rajout des tris et affichages des catégories dans menu.js

--- js/menus.js
+++ js/menus.js
@@ -4,6 +4,64 @@ const starters = document.getElementById('starters');
 const dishes = document.getElementById('dishes');
 const desserts = document.getElementById('desserts');
 
+// fonction de tri sur les titres
+function filterMenu(str_menu) { // string en param lancé sur le html cartes-menus.php
+    console.log("menu selectionné : " + str_menu);
+    switch (str_menu) {
+        case 'all': {
+            showMenu(drinks);
+            showMenu(starters);
+            showMenu(dishes);
+            showMenu(desserts);
+        }
+        break;
+        case 'drinks': {
+            showMenu(drinks);
+            maskMenu(starters);
+            maskMenu(dishes);
+            maskMenu(desserts);
+        } 
+        break;
+        case 'starters': {
+            showMenu(starters);
+            maskMenu(drinks);
+            maskMenu(dishes);
+            maskMenu(desserts);
+        } 
+        break;
+        case 'dishes': {
+            showMenu(dishes);
+            maskMenu(drinks);
+            maskMenu(starters);
+            maskMenu(desserts);
+        } 
+        break;
+        case 'desserts': {
+            showMenu(desserts);
+            maskMenu(starters);
+            maskMenu(drinks);
+            maskMenu(dishes);
+            
+        } 
+        break;
+    }
+    
+}
+
+// fonction  d affichage des menus
+function showMenu(menu) {
+    if (menu.classList.contains('none')){
+        menu.classList.remove('none');
+    } 
+}
+
+//fonction de masquage des menus 
+function maskMenu (menu) {
+    if (!menu.classList.contains('none')){
+        menu.classList.add('none');
+    } 
+}
+
 // Fonction pour afficher les images avec pagination
 function showImagesWithPagination(imagesData, container) {
     const itemsPerPage = 1;
